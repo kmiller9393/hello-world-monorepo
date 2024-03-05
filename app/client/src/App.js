2aae56a90ebb8b60c7942e1b25cecb53da3d127d
Kurt Miller <kmiller9393@gmail.com>
2024-03-04 22:21:34 -0700

    update boilerplate code

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const getHelloWorld = async () => {
+    const response = await fetch("http://localhost:5757");
+    const result = await response.json();
+
+    console.log(result);
+  };
+
+  useEffect(() => {
+    getHelloWorld();
+  }, []);
+
   return (
     <div className="App">
       <main className="main">
-        <p>Hello World</p>
+        <h2>Hello World</h2>
       </main>
     </div>
   );
--- app/server/index.js
+++ app/server/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 
+const PORT = 5757;
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World From Express!");
+  res.json({ message: "Hello World from the server!" });
 });
 
-app.listen(5757, () => {
-  console.log("Server is running on http://localhost:5757");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
